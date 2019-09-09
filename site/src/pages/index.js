458ddf15d47267c6b92ec067d823ad393e675e9c
hxgdzyuyi <hxgdzyuyi@gmail.com>
2019-09-09 11:01:47 +0800

    添加日报标题

--- site/src/pages/index.js
+++ site/src/pages/index.js
@@ -5,6 +5,20 @@ import SEO from "../components/seo"
 import { useStaticQuery, graphql } from "gatsby"
 import './index.sass'
 
+const NAMESPACE_AND_LABEL = {
+  '3dm': '3DM',
+  '17173': '17173',
+  'gamersky': '游民星空',
+  'duowan': '多玩',
+}
+
+const NAMESPACE_AND_URL = {
+  '3dm': 'https://www.3dmgame.com',
+  '17173': 'https://www.17173.com',
+  'gamersky': 'https://www.gamersky.com',
+  'duowan': 'http://www.duowan.com',
+}
+
 const HotLink = ({ link }) => {
   return (
     <h3 className="info-tit">
@@ -66,7 +80,14 @@ const DailyBlock = ({ daily }) => {
   return (
     <div className="daily-block">
       <div className="hd">
-        <h2>{ daily.pk }</h2>
+        <h2>
+          <a className="daily-title-link"
+            rel="noopener noreferrer"
+            target="_blank"
+            href={ NAMESPACE_AND_URL[daily.pk] }>
+            { NAMESPACE_AND_LABEL[daily.pk] }
+          </a>
+        </h2>
       </div>
 
       <div className="bd">
